f0f0bf516630ad9eb548ae44cdd4d5ee44348b97
Michael Tutt <tutt.michael@gmail.com>
2018-02-24 15:57:14 -0800

    Create boilerplate text for dev

---
+++ src/components/Boilerplate.vue
@@ -0,0 +1,36 @@
+/* eslint-disable */
+<template>
+<div>
+  <p>
+    Lorem ipsum dolor sit amet, id aeterno scripserit mea.
+    Mel eu euripidis mediocritatem.
+    Pri persecuti consectetuer an,
+    usu lorem scaevola definiebas no,
+    cu tantas suscipit quo. Te vel odio fugit deterruisset, in omnesque indoctum volutpat qui.
+    Ad nec expetendis moderatius.
+  </p>
+  <p>
+    Vidisse bonorum sanctus nec an, ad pri aeque discere sensibus.
+    Vim id eirmod consequuntur.
+    Ea libris recteque vel, id sed vocent bonorum, ne sea cetero adipiscing assueverit.
+    Ex scaevola percipitur vel.
+    His harum eloquentiam comprehensam eu, eu omnium expetenda quo,
+    mel aperiam electram definiebas ne.
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'boilerplate',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../static/_main';
+
+div, p {
+  font-family: $family-secondary !important;
+  padding: 1rem 0;
+}
+</style>
