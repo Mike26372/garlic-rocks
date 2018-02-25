be981723b4c765265dcb23430eb16a13271d1de1
Michael Tutt <tutt.michael@gmail.com>
2018-02-24 17:02:48 -0800

    Fix navbar to top of page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item link" href="/">
       <LogoSmall class="garlic-logo-small" />
