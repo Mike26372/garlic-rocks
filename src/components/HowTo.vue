895a6050c6fa56c1f5d972834a1d22dd76b7352c
Michael Tutt <tutt.michael@gmail.com>
2018-05-10 08:15:09 -0700

    Remove periods

--- src/components/HowTo.vue
+++ src/components/HowTo.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
-  <p>Remove the cloves off the garlic head and separate on a flat surface.</p>
-  <p>Hold your Garlic Rock firmly in your hand and smash down on the cloves.</p>
-  <p>Now you can easily pick off the loose papery skin from the garlic.</p>
-  <p>Clean the rock easily in hot soapy water or put it in the dishwasher.</p>
+  <p>Remove the cloves off the garlic head and separate on a flat surface</p>
+  <p>Hold your Garlic Rock firmly in your hand and smash down on the cloves</p>
+  <p>Now you can easily pick off the loose papery skin from the garlic</p>
+  <p>Clean the rock easily in hot soapy water or put it in the dishwasher</p>
 </div>
 </template>
 
