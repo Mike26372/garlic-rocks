e1ad5f6702cf4fdeb2d4738051e5e026f733184d
Michael Tutt <tutt.michael@gmail.com>
2018-05-20 15:07:49 -0700

    Update HowTo with icons

--- src/components/HowTo.vue
+++ src/components/HowTo.vue
@@ -1,15 +1,40 @@
 <template>
-<div>
-  <p>Remove the cloves off the garlic head and separate on a flat surface</p>
-  <p>Hold your Garlic Rock firmly in your hand and smash down on the cloves</p>
-  <p>Now you can easily pick off the loose papery skin from the garlic</p>
-  <p>Clean the rock easily in hot soapy water or put it in the dishwasher</p>
+<div class="how-to-container">
+  <div class="how-to">
+    <div class="how-to-icon-container">
+      <One class="how-to-icon" />
+    </div>
+    <span>Remove the cloves off the garlic head and separate on a flat surface</span>
+  </div>
+  <div class="how-to">
+    <div class="how-to-icon-container">
+      <Two class="how-to-icon" />
+    </div>
+    <span>Hold your Garlic Rock firmly in your hand and smash down on the cloves</span>
+  </div>
+  <div class="how-to">
+    <div class="how-to-icon-container">
+      <Three class="how-to-icon" />
+    </div>
+    <span>Now you can easily pick off the loose papery skin from the garlic</span>
+  </div>
+  <div class="how-to">
+    <div class="how-to-icon-container">
+      <Four class="how-to-icon" />
+    </div>
+    <span>Clean the rock easily in hot soapy water or put it in the dishwasher</span>
+  </div>
 </div>
 </template>
 
 <script>
+import One from '@/assets/one.svg';
+import Two from '@/assets/two.svg';
+import Three from '@/assets/three.svg';
+import Four from '@/assets/four.svg';
 export default {
   name: 'howto',
+  components: { One, Two, Three, Four },
 };
 </script>
 
@@ -22,5 +47,36 @@ div, p {
   font-weight: 300;
   padding: 1.5rem 0;
   text-align: center;
+}
+
+.how-to-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.how-to {
+  width: 50%;
+  padding: 0 1em;
+}
+
+.how-to-icon-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.how-to-icon {
+  width: 150px;
+  padding-bottom: .5em;
+}
+
+@media (max-width: $tablet) {
+  .how-to {
+    width: 100%;
+  }
+
+  .how-to-icon {
+    width: 100px;
+  }
 }
 </style>
